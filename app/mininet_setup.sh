ce8afa558fd27a7a1306920fd78d2f1a0dbb24ed
PluT00 <314lut00@gmail.com>
2025-03-25 11:44:31 +0300

    Update mininet_setup.sh

--- app/mininet_setup.sh
+++ app/mininet_setup.sh
@@ -3,7 +3,7 @@ h2 ifconfig h2-eth0 10.0.1.2/24
 r1 ifconfig r1-eth0 10.0.1.254/24
 r1 ifconfig r1-eth2 10.0.2.1/24
 r2 ifconfig r2-eth0 10.0.2.2/24
-r2 ifconfig r2-eth1 10.0.3.254/24
+r2 ifconfig r2-eth2 10.0.3.254/24
 h3 ifconfig h3-eth0 10.0.3.1/24
 s1 ifconfig s1-eth0 10.0.3.2/24
 
