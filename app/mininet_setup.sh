f874e659c47403cc76d420fe4d72863c3781fea4
PluT00 <314lut00@gmail.com>
2025-03-25 11:47:59 +0300

    Update mininet_setup.sh

--- app/mininet_setup.sh
+++ app/mininet_setup.sh
@@ -1,11 +1,11 @@
-h1 ifconfig h1-eth0 10.0.1.1/24
-h2 ifconfig h2-eth0 10.0.1.2/24
-r1 ifconfig r1-eth0 10.0.1.254/24
-r1 ifconfig r1-eth2 10.0.2.1/24
-r2 ifconfig r2-eth0 10.0.2.2/24
-r2 ifconfig r2-eth2 10.0.3.254/24
-h3 ifconfig h3-eth0 10.0.3.1/24
-s1 ifconfig s1-eth0 10.0.3.2/24
+h1 ifconfig h1-eth0 10.0.1.1/24 up
+h2 ifconfig h2-eth0 10.0.1.2/24 up
+r1 ifconfig r1-eth0 10.0.1.254/24 up
+r1 ifconfig r1-eth2 10.0.2.1/24 up
+r2 ifconfig r2-eth0 10.0.2.2/24 up
+r2 ifconfig r2-eth2 10.0.3.254/24 up
+h3 ifconfig h3-eth0 10.0.3.1/24 up
+s1 ifconfig s1-eth0 10.0.3.2/24 up
 
 h1 ip route add default via 10.0.1.254
 h2 ip route add default via 10.0.1.254
